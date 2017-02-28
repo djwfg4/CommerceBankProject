6467d57507539e165ed0e1bb29307c0a617f8b62
jwhipple07 <jwhipple07@gmail.com>
2017-02-27 23:14:33 -0600

    Technical Prototype upload

---
+++ BudgetingApplication/BudgetingApplication/Controllers/DBClientController.cs
@@ -0,0 +1,28 @@
+﻿using BudgetingApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BudgetingApplication.Controllers
+{
+    public class DBClientController : Controller
+    {
+        static DatabaseContext dbContext = new DatabaseContext();
+        
+        // GET: Client
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public Client getClient()
+        {
+           
+
+            Client client = dbContext.Clients.Single(cl => cl.ClientID == 1);
+            return client;
+        }
+    }
+}
---
+++ BudgetingApplication/BudgetingApplication/Controllers/DBTransactionsController.cs
@@ -0,0 +1,30 @@
+﻿using BudgetingApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace BudgetingApplication.Controllers
+{
+    public class DBTransactionsController : Controller
+    {
+        DatabaseContext dbContext = new DatabaseContext();
+
+        // GET: Transactions
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult getTransactions()
+        {
+            var transactions = dbContext.Transactions.Where(
+                trans => trans.TransactionAccountNo == 2 ||
+                trans.TransactionAccountNo == 3).ToArray();
+            
+            return Json(transactions, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
