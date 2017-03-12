0856ae9bf13910f883a9d3e5b70a64d0b4dd1bec
DRock <DRock@DESKTOP-PJG3IKB>
2017-03-11 19:48:27 -0600

    First commit

---
+++ BudgetingApplication/BudgetingApplication/Controllers/TransactionsController.cs
@@ -0,0 +1,24 @@
+﻿using BudgetingApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BudgetingApplication.Controllers
+{
+    public class TransactionsController : Controller
+    {
+        private DatabaseContext dbContext = new DatabaseContext();
+
+        // GET: Transactions
+        public ActionResult Index()
+        {
+
+            List<Transaction> transactionList = new List<Transaction>();
+            transactionList = dbContext.Transactions.ToList();
+
+            return View(transactionList);
+        }
+    }
+}
