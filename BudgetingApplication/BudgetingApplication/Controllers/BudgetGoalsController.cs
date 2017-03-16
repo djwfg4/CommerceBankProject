6606ecd83c177aae50d03a04e52d965ba603e81d
jwhipple07 <jwhipple07@gmail.com>
2017-03-15 22:39:52 -0500

    Added an insert view for goals.

--- BudgetingApplication/BudgetingApplication/Controllers/BudgetGoalsController.cs
+++ BudgetingApplication/BudgetingApplication/Controllers/BudgetGoalsController.cs
@@ -10,16 +10,37 @@ namespace BudgetingApplication.Controllers
     public class BudgetGoalsController : Controller
     {
         private DataContext dbContext = new DataContext();
+        private static int CLIENT_ID = 1;
+
         // GET: BudgetGoals
         public ActionResult Index()
         {
 
             List<BudgetGoals_VW> BudgetGoalList = new List<BudgetGoals_VW>();
 
-            
-             BudgetGoalList = dbContext.BudgetGoals_VW.ToList();
 
+            BudgetGoalList = dbContext.BudgetGoals_VW.Where(x => x.ClientID == CLIENT_ID).ToList();
+           
             return View(BudgetGoalList.ToList());
+        }
+
+        public ActionResult InsertBudgetGoal()
+        {
+            ViewBag.GoalCategory = dbContext.Categories.Select(x => new SelectListItem()
+            {
+                Text = x.CategoryType,
+                Value = x.CategoryID.ToString()
+            }).OrderBy(v => v.Text);
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult InsertBudgetGoal(BudgetGoal id)
+        {
+            //BudgetGoal Add_Brand = new BudgetGoal();
+            //dbContext.BudgetGoals.Add(Add_Brand);
+            //dbContext.SaveChanges();
+            return RedirectToAction("index");
         }
     }
 }
