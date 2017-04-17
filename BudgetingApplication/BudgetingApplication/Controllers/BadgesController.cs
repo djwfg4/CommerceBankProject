050eef03d5593bee4c572ecb4fadfbea7d7c5fc8
A-Sweeney <adsxw3@mail.umkc.edu>
2017-04-16 20:58:52 -0500

    Add files via upload
    Added function to retrieve badges client has achieved

--- BudgetingApplication/BudgetingApplication/Controllers/BadgesController.cs
+++ BudgetingApplication/BudgetingApplication/Controllers/BadgesController.cs
@@ -3,15 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BudgetingApplication.ViewModels;
+using BudgetingApplication.Models;
+using System.Data.Entity;
 
 namespace BudgetingApplication.Controllers
 {
     public class BadgesController : Controller
     {
+        private DataContext dbContext = new DataContext();
+        private static int CLIENT_ID = 1;
+
         // GET: Badges
         public ActionResult Index()
         {
-            return View();
+            ViewBag.NumColumns = 10;
+
+            BadgesModelView badgeModel = new BadgesModelView();
+            badgeModel.badgeNames = getUserBadges();
+            return View(badgeModel);
         }
+
+        private List<String> getUserBadges()
+        {
+            List<ClientBadge> ClientBadgeList = new List<ClientBadge>();
+            ClientBadgeList = dbContext.ClientBadges.Where(x => x.ClientID == CLIENT_ID).ToList();
+            List<Badge> TotalBadges = dbContext.Badges.ToList();
+
+            List<String> returnList = new List<String>();
+
+            for(int i = 0; i < ClientBadgeList.Count(); i++)
+            {
+                for(int j = 0; j < TotalBadges.Count(); j++)
+                {
+                    if(ClientBadgeList[i].BadgeID == TotalBadges[j].BadgeID)
+                    {
+                        String temp = TotalBadges[j].BadgeName;
+                        temp += ".png";
+                        returnList.Add(TotalBadges[j].BadgeName );
+                    }
+                }
+            }
+
+            return returnList;
+        }
+
+        
     }
 }
