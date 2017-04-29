5b2e7e2e9c9da1071d79cb6e99d89b74f71333da
jwhipple07 <jwhipple07@gmail.com>
2017-04-28 22:06:13 -0500

    SavingsGoal files

--- BudgetingApplication/BudgetingApplication/ViewModels/SavingsGoalsViewModel.cs
+++ BudgetingApplication/BudgetingApplication/ViewModels/SavingsGoalsViewModel.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using BudgetingApplication.Models;
+using System.Web.Mvc;
 
 namespace BudgetingApplication.ViewModels
 {
     public class SavingsGoalsViewModel
     {
-        List<SavingsGoal> savingsGoals;
+        public List<SavingsGoal> savingsView { get; set; }
+        public SavingsGoal savingsGoal { get; set; }
+        public List<SelectListItem> active { get; set; }
+        public double totalBudgeted { get; set; }
+        public double totalSaved { get; set; }
+        string progressType { get; set; }
+
+        public int getPercentage()
+        {
+            double percentage = totalSaved / totalBudgeted;
+            return Math.Abs((int)(percentage * 100));
+        }
     }
 }
